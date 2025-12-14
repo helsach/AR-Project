5dfc23dba05e707528963918d2bc1edf111c3a6c
HELSA CHRISTABEL HARSONO <christabelhelsa@gmail.com>
2025-12-14 10:17:53 +0700

    benerin asset quiz

---
+++ Assets/VuforiaLicense.cs
@@ -0,0 +1,14 @@
+
+            //This class is auto-generated, please do not overwrite
+            using UnityEngine.Scripting;
+
+            public class VuforiaLicense
+            {
+const string license = "QVVMc1B1Ny8vLy8vQUFBQm1XcWx0cHZaRWs2UXN2UlM4dTNPRGZjaHdLYmJzeit3VzJQRG9lSmtpaTBrdEVGZ1lCUU5XU25iNkZ1cHZYLzQrQ3FRQWdVa0xydHV5aXFxU1EzbUxMZTJRVGVXQWY1MFB4YVRGWC9Gd0QySjdHWFIrZEJWMjNlZWoyYkpCa0dCKzRiblJXSXdQZk1oSEh2b2RGK2RFUzVJMGw3MFBpRVlTdUpkNXljMDRJS3psOVVnYmF2QW1NM0t4SSs4RjJJR0Myd3BFM1RoQnh0Rm9YbVFCcW1MVzRNc2NBRnNUQmJMYUFOUUtUbVcyampyZXFsUUZUUjV0RWFmOHZMYXB2eStyaytBdXdMWExaZ1lKdlZtR25Vb2N2cWZtZldTaWY3ZjZ3NFFJM2JvWVNUVUw5VkVQVVpiTmVzZEZSZlgyTTNEclNEMnNrMjM3c25KOVdaaDdIK2dDK2ZtOVl1bjFJNFVDdW53cVdUTjhpZDA=";
+
+                [Preserve]
+                static string GetLicenseKey()
+                {
+                    return license;
+                }
+            }
